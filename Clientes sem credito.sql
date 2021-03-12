79fa0fcc8f00dffac16b591590795ef04213198b
cirojunior <cirojunior@gmail.com>
2021-03-12 14:20:28 -0300

    Comentário query

--- Clientes sem credito.sql
+++ Clientes sem credito.sql
@@ -1,3 +1,5 @@
+--No sistema mercanet fazendo a consulta dos clientes com pedido e sem limite de crédito para avaliação do financeiro.
+
 select distinct DB_PED_CLIENTE, DB_CLI_NOME
 from DB_PEDIDO, DB_PEDIDO_COMPL, DB_CLIENTE
 where DB_PEDIDO.DB_PED_NRO = DB_PEDIDO_COMPL.DB_PEDC_NRO
